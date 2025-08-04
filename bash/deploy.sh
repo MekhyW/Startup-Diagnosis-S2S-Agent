c4e171156ddc7e89bfc483d1c83bff67917c7ebb
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2025-08-04 15:31:41 -0300

    Update deploy.sh

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -11,5 +11,4 @@ aws ecs create-service \
   --service-name startup-diagnosis-service \
   --task-definition startup-diagnosis-agent:1 \
   --desired-count 1 \
-  --launch-type FARGATE \
-  --network-configuration "awsvpcConfiguration={subnets=[subnet-0e3d8d587027318f6,subnet-0e420769b610bf125],securityGroups=[sg-07e269e98958c89ca],assignPublicIp=ENABLED}"
+  --launch-type FARGATE
